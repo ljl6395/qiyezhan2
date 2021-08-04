9adc6efd54ca74431c94106e22457977bd11ed69
liujianli <376122478@qq.com>
2021-08-04 10:25:38 +0800

    地图优化

--- js/about.js
+++ js/about.js
@@ -6,10 +6,22 @@ axios.get('./router/about.json')
     `)
 
     var map = new BMap.Map("allmap");
-    var point = new BMap.Point(119.247957,26.060076);
-    var marker = new BMap.Marker(point);  // 创建标注
-    map.addOverlay(marker);              // 将标注添加到地图中
-    map.centerAndZoom(point, 20);
+    // var point = new BMap.Point(119.247957,26.060076);
+    // var marker = new BMap.Marker(point);  // 创建标注
+    // map.addOverlay(marker);              // 将标注添加到地图中
+    // map.centerAndZoom(point, 20);
+    // map.enableScrollWheelZoom(true);
+    map.centerAndZoom(new BMap.Point(119.33022111, 26.0471255), 11);
+    var local = new BMap.LocalSearch(map, {
+      renderOptions: { map: map }
+    });
+    local.search("福州西湖公园");
+    map.addControl(new BMap.MapTypeControl({
+      mapTypes: [
+        BMAP_NORMAL_MAP,
+        BMAP_HYBRID_MAP
+      ]
+    }));
     map.enableScrollWheelZoom(true);
 
 
@@ -21,4 +33,4 @@ axios.get('./router/about.json')
         </p>
       `)
     })
-})
+  })
